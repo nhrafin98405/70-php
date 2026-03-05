5f43538a0a1a3f14c204e57969c0ba4724a84beb
niazhasanrafin <nhrafin98405.com>
2026-03-05 10:18:28 +0600

    add

---
+++ basic/home-2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php</title>
+</head>
+<body>
+
+<?php 
+
+$date = "05-march-2026";
+
+?>
+
+
+
+<h1>today is <?= $date; ?></h1>
+    
+</body>
+</html>
