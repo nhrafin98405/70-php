27991537ea8a9ea4a579858dfb18e67b852d875c
niazhasanrafin <nhrafin98405.com>
2026-03-07 12:07:15 +0600

    add

---
+++ operator/increment_decrement.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+$e = 10;
+
+echo ++$e;
+
+echo"<br>";
+
+$r = 10;
+
+echo --$r;
+
+echo"<br>";
+
+$t = 45;
+
+echo $t++;
+
+echo"<br>";
+
+echo"<br>";
+?>
---
+++ statement/if-else.php
@@ -0,0 +1,12 @@
+<?php
+
+$a =75;
+$b =4857;
+
+if($a<60){
+    echo "true";
+}else{
+    echo "false";
+}
+
+?>
